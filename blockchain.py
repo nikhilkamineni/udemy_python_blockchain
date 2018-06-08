823192987e2b46458f74d50197e5021bb71e1435
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-06-08 01:12:13 -0400

    Added output participants menu option

--- blockchain.py
+++ blockchain.py
@@ -6,6 +6,7 @@ genesis_block = {
 blockchain = [genesis_block]
 open_transactions = []
 owner = 'Nikhil'
+participants = {'Nikhil'}
 
 
 def hash_block(block):
@@ -28,6 +29,8 @@ def add_transaction(recipient, sender=owner, amount=1.0):
         'amount': amount
     }
     open_transactions.append(transaction)
+    participants.add(sender)
+    participants.add(recipient)
 
 
 def mine_block():
@@ -94,6 +97,7 @@ while waiting_for_input:
     print(' 1: Enter a new transaction value')
     print(' 2: Mine a new block')
     print(' 3: Output the blockchain blocks')
+    print(' 4: Output participants')
     print(' h: Manipulate the chain')
     print(' q: Quit')
     user_choice = get_user_choice()
@@ -114,6 +118,8 @@ while waiting_for_input:
                 'index': 0,
                 'transactions': [{'sender': 'Zephyr', 'recipient': 'Pickle', 'amount': 100}]
             }
+    elif user_choice == '4':
+        print(participants)
     elif user_choice == 'q':
         waiting_for_input = False
     else:
